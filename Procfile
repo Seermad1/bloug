1c6dcd3b67cf4a0c4145fe1861852b507f71d0d4
Abdulsamad <100522473+Seermad1@users.noreply.github.com>
2022-07-08 10:54:19 +0100

    Add files via upload

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn To_do.wsgi --log-file -
+web: gunicorn config.wsgi --log-file -
--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = ["127.0.0.1","bloug.herokuapp.com"]
+ALLOWED_HOSTS = ["bloug.herokuapp.com", "127.0.0.1"]
 
 
 # Application definition
